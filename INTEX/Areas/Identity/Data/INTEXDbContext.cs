97816a3ac20bd6d9e2ad199faf10f3b6cf4860a4
James G <jpgrayson21@gmail.com>
2022-03-24 16:52:52 -0600

    Add project files.

---
+++ INTEX/Areas/Identity/Data/INTEXDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using INTEX.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace INTEX.Data
+{
+    public class INTEXDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public INTEXDbContext(DbContextOptions<INTEXDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
