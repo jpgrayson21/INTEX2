0ab2dbe8390af4cbeb7ac2c2ace405d8d160fe2c
joshmbrown <79293664+joshmbrown@users.noreply.github.com>
2022-03-25 18:10:04 -0600

    Login functionality

--- INTEX/Startup.cs
+++ INTEX/Startup.cs
@@ -1,6 +1,8 @@
+using INTEX.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,12 @@ namespace INTEX
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddDbContext<INTEXDbContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:INTEXDbContextConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +52,7 @@ namespace INTEX
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +60,7 @@ namespace INTEX
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
