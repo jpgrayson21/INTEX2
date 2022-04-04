d1b2f021fe8dcde9a1766fed8904a38a7b15659b
Brandon <brandonjmhunt@gmail.com>
2022-04-04 13:47:58 -0600

    hc 2 no nugpg

--- INTEX/Models/DbHelper.cs
+++ INTEX/Models/DbHelper.cs
@@ -10,20 +10,20 @@ namespace INTEX.Models
 {
     public static class DbHelper
     {
-        public static string GetRDSConnectionString()
-        {
-            var appConfig = ConfigurationManager.AppSettings;
+        //public static string GetRDSConnectionString()
+        //{
+        //    var appConfig = ConfigurationManager.AppSettings;
 
-            string dbname = appConfig["RDS_DB_NAME"];
+        //    string dbname = appConfig["RDS_DB_NAME"];
 
-            if (string.IsNullOrEmpty(dbname)) return null;
+        //    if (string.IsNullOrEmpty(dbname)) return null;
 
-            string username = appConfig["RDS_USERNAME"];
-            string password = appConfig["RDS_PASSWORD"];
-            string hostname = appConfig["RDS_HOSTNAME"];
-            string port = appConfig["RDS_PORT"];
+        //    string username = appConfig["RDS_USERNAME"];
+        //    string password = appConfig["RDS_PASSWORD"];
+        //    string hostname = appConfig["RDS_HOSTNAME"];
+        //    string port = appConfig["RDS_PORT"];
 
-            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
-        }
+        //    return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+        //}
     }
 }
