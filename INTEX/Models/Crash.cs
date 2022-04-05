71dd67e9f0ae6340915d0e0592f10810f404ec88
joshmbrown <79293664+joshmbrown@users.noreply.github.com>
2022-04-05 17:47:20 -0600

    Add Crash

--- INTEX/Models/Crash.cs
+++ INTEX/Models/Crash.cs
@@ -8,33 +8,82 @@ namespace INTEX.Models
         [Key]
         [Required]
         public int CRASH_ID { get; set; }
+
+        [Required]
         public DateTime CRASH_DATETIME { get; set; }
-        public string ROUTE { get; set; }
+
+        [Required]
+        public string MAIN_ROAD_NAME { get; set; }
+
+        [Required]
+        public string CITY { get; set; }
+
+        [Required]
+        public string COUNTY_NAME { get; set; }
+
+        [Required]
+        public int CRASH_SEVERITY_ID { get; set; }
+
+        [Required]
+        public bool WORK_ZONE_RELATED { get; set; }
+
+        [Required]
+        public bool PEDESTRIAN_INVOLVED { get; set; }
+
+        [Required]
+        public bool BICYCLIST_INVOLVED { get; set; }
+
+        [Required]
+        public bool MOTORCYCLE_INVOLVED { get; set; }
+
+        [Required]
+        public bool IMPROPER_RESTRAINT { get; set; }
+
+        [Required]
+        public bool UNRESTRAINED { get; set; }
+
+        [Required]
+        public bool DUI { get; set; }
+
+        [Required]
+        public bool INTERSECTION_RELATED { get; set; }
+
+        [Required]
+        public bool WILD_ANIMAL_RELATED { get; set; }
+
+        [Required]
+        public bool DOMESTIC_ANIMAL_RELATED { get; set; }
+
+        [Required]
+        public bool OVERTURN_ROLLOVER { get; set; }
+
+        [Required]
+        public bool COMMERCIAL_MOTOR_VEH_INVOLVED { get; set; }
+
+        [Required]
+        public bool TEENAGE_DRIVER_INVOLVED { get; set; }
+
+        [Required]
+        public bool OLDER_DRIVER_INVOLVED { get; set; }
+
+        [Required]
+        public bool NIGHT_DARK_CONDITION { get; set; }
+
+        [Required]
+        public bool SINGLE_VEHICLE { get; set; }
+
+        [Required]
+        public bool DISTRACTED_DRIVING { get; set; }
+
+        [Required]
+        public bool DROWSY_DRIVING { get; set; }
+
+        [Required]
+        public bool ROADWAY_DEPARTURE { get; set; }
+
         public double MILEPOINT { get; set; }
+        public string ROUTE { get; set; }
         public double LAT_UTM_Y { get; set; }
         public double LONG_UTM_X { get; set; }
-        public string MAIN_ROAD_NAME { get; set; }
-        public string CITY { get; set; }
-        public string COUNTY_NAME { get; set; }
-        public int CRASH_SEVERITY_ID { get; set; }
-        public bool WORK_ZONE_RELATED { get; set; }
-        public bool PEDESTRIAN_INVOLVED { get; set; }
-        public bool BICYCLIST_INVOLVED { get; set; }
-        public bool MOTORCYCLE_INVOLVED { get; set; }
-        public bool IMPROPER_RESTRAINT { get; set; }
-        public bool UNRESTRAINED { get; set; }
-        public bool DUI { get; set; }
-        public bool INTERSECTION_RELATED { get; set; }
-        public bool WILD_ANIMAL_RELATED { get; set; }
-        public bool DOMESTIC_ANIMAL_RELATED { get; set; }
-        public bool OVERTURN_ROLLOVER { get; set; }
-        public bool COMMERCIAL_MOTOR_VEH_INVOLVED { get; set; }
-        public bool TEENAGE_DRIVER_INVOLVED { get; set; }
-        public bool OLDER_DRIVER_INVOLVED { get; set; }
-        public bool NIGHT_DARK_CONDITION { get; set; }
-        public bool SINGLE_VEHICLE { get; set; }
-        public bool DISTRACTED_DRIVING { get; set; }
-        public bool DROWSY_DRIVING { get; set; }
-        public bool ROADWAY_DEPARTURE { get; set; }
     }
 }
